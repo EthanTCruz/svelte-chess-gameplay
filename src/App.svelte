1f79bfce0bf4565dee662af7a824f0c43ee3793d
EthanYaMachafuko <89563202+EthanYaMachafuko@users.noreply.github.com>
2022-07-26 09:52:10 -0500

    first

---
+++ src/App.svelte
@@ -0,0 +1,344 @@
+<script>
+
+	import { board } from './store.js'	
+
+	let turn = true;
+	let move_no;
+	let positions_selected = 0;
+	let move_from;
+	let move_to;
+	function checkMove(){
+		let piece = board[move_from[1]].positions[move_from[0]]
+		
+		let team = piece.charAt(piece.length - 1);
+		piece = piece.charAt(0);
+		console.log(team)
+		console.log(piece)
+		if (piece == 'P'){
+			return checkPawn(piece,team)
+		} else if (piece == 'N'){
+			return checkKnight(piece,team)
+		} else if (piece == 'R') {
+			return checkLaterally(team)
+		}
+		return true
+	}
+
+	function checkLaterally(team){
+		let x = move_from[0] - move_to[0]
+		let y = move_from[1] - move_to[1]
+		if (x == 0){
+
+		let direction
+		if (y < 0){
+			direction = -1
+		} else if (y > 0){
+			direction = 1
+		} else {
+			return false
+		}
+
+			return checkDirection(team,direction,1,move_from)	
+		} else 	if (y == 0){
+
+		let direction
+		if (x < 0){
+			direction = -1
+		} else if (x > 0){
+			direction = 1
+		} else {
+			return false
+		}
+
+			return checkDirection(team,direction,0,move_from)	
+		} else {
+			return false
+		}
+			
+	}
+	function checkDirection(team,direction,axis,location){
+		let opponent
+		console.log(team,direction,axis,location)
+		if (team == 'W') {
+			opponent = 'B'
+		} else {
+			opponent = 'W'
+		}
+
+		if (direction == 1){
+			if (!(location[axis] <= 6)){
+				console.log(5)
+				return false
+			}
+		} else 	if (direction == -1){
+			if (!(location[axis] >= 1)){
+				console.log(6)
+				return false
+			}
+		}
+		console.log(0)
+		let next = location[axis] + direction
+
+		
+		
+		let next_space_team = board[next[1]].positions[next[0]].charAt(1)
+		if (next_space_team == opponent){
+			if (next[0] == move_to[0] && next[1] == move_to[1]){
+				return true
+			}
+			console.log(1)
+			return false
+		} else if (next_space_team == ' '){
+			if (next[0] == move_to[0] && next[1] == move_to[1]){
+				return true
+			}
+			console.log(2)
+			return checkDirection(team,direction,axis,next)
+		} else { 
+			return true
+		}
+
+	}
+
+
+	function checkKnight(piece,team){
+		let opponent;
+		if (team == 'W'){
+			opponent = 'B'
+		} else {
+			opponent = 'W'
+		}
+		let r2_check = move_from[0] >= 0 && move_from[0] <= 5;
+		let l2_check = move_from[0] >= 2 && move_from[0] <= 7;
+		let d2_check = move_from[1] >= 2 && move_from[1] <= 7;
+		let u2_check = move_from[1] >= 0 && move_from[1] <= 5;
+		let r1_check = move_from[0] >= 0 && move_from[0] <= 6;
+		let l1_check = move_from[0] >= 1 && move_from[0] <= 7;
+		let d1_check = move_from[1] >= 1 && move_from[1] <= 7;
+		let u1_check = move_from[1] >= 0 && move_from[1] <= 6;
+		console.log(`r1: ${r1_check}`)
+		
+		let condition;
+
+		if (r2_check) {
+			let r_condition = move_from[0] + 2 == move_to[0] 
+			if (d1_check){
+				condition =  r_condition && move_from[0] + 2 == move_to[0]
+				
+				condition =  r_condition && move_from[1] - 1  == move_to[1]
+				
+				condition = condition && board[move_to[1]].positions[move_to[0]].charAt(1) != team
+				
+				if(condition){
+					return true
+				} 
+			} 
+			if (u1_check) {
+				condition =  r_condition && move_from[0] + 2 == move_to[0]
+				condition =  r_condition && move_from[1] + 1  == move_to[1]
+				condition = condition && board[move_to[1]].positions[move_to[0]].charAt(1) != team
+				if(condition){
+					return true
+				}
+			}
+		} 
+
+		if (l2_check) {
+			let l_condition = move_from[0] - 2 == move_to[0] 
+			if (d1_check){
+				condition =  l_condition && move_from[0] - 2 == move_to[0]
+				condition =  l_condition && move_from[1] - 1  == move_to[1]
+				condition = condition && board[move_to[1]].positions[move_to[0]].charAt(1) != team
+				if(condition){
+					return true
+				}
+			} 
+			if (u1_check) {
+				condition =  l_condition && move_from[0] + 2 == move_to[0]
+				condition =  l_condition && move_from[1] + 1  == move_to[1]
+				condition = condition && board[move_to[1]].positions[move_to[0]].charAt(1) != team
+				if(condition){
+					return true
+				}
+			}
+		
+		} 
+
+		 if (u2_check) {
+			
+			let u_condition = move_from[1] + 2 == move_to[1] 
+			if (l1_check){
+				
+				condition =  u_condition && move_from[1] + 2 == move_to[1]
+				
+				condition =  u_condition && move_from[0] - 1  == move_to[0]
+				
+				condition = condition && board[move_to[1]].positions[move_to[0]].charAt(1) != team
+				
+				if(condition){
+					return true
+				}
+			} 
+			if (r1_check) {
+				condition =  u_condition && move_from[1] + 2 == move_to[1]
+				console.log(condition)
+				condition =  u_condition && move_from[0] + 1  == move_to[0]
+				console.log(condition)
+				condition = condition && board[move_to[1]].positions[move_to[0]].charAt(1) != team
+				if(condition){
+					return true
+				}
+			}
+		
+		}  
+		 if (d2_check) {
+			
+			let d_condition = move_from[1] - 2 == move_to[1] 
+			if (l1_check){
+				condition =  d_condition && move_from[1] - 2 == move_to[1]
+				condition =  d_condition && move_from[0] - 1  == move_to[0]
+				condition = condition && board[move_to[1]].positions[move_to[0]].charAt(1) != team
+				if(condition){
+					return true
+				}
+			} 
+			 if (r1_check) {
+				condition =  d_condition && move_from[1] - 2 == move_to[1]
+				console.log(condition)
+				condition =  d_condition && move_from[0] + 1  == move_to[0]
+				console.log(condition)
+				condition = condition && board[move_to[1]].positions[move_to[0]].charAt(1) != team
+				if(condition){
+					return true
+				}
+			}
+		
+		}
+
+
+		return false
+
+
+	}
+
+	function checkPawn(piece,team){
+		let direction;
+		let opponent;
+		let row_check = move_from[1] >= 1 && move_from[1] <= 6
+		let cond1
+		let cond2
+		let cond3
+		let cond4
+		let right_check
+		let left_check 
+		if (team == 'W'){
+			direction = 1
+			opponent = 'B'
+			cond1 = move_from[1]+2*direction == move_to[1] && move_from[1] == 1
+		} else {
+			direction = -1
+			opponent = 'W'
+			cond1 = move_from[1]+2*direction == move_to[1] && move_from[1] == 6
+
+		}
+
+		
+		right_check = 1 <= move_from[0]+1 && move_from[0]+1 <= 7
+		right_check = right_check && board[move_to[1]].positions[move_to[0]].charAt(1) == opponent
+		left_check = 2 <= move_from[0]+1 && move_from[0]+1 <= 8
+		left_check = left_check && board[move_to[1]].positions[move_to[0]].charAt(1) == opponent
+		
+		cond2 = move_from[1]+1*direction == move_to[1] && move_from[0] == move_to[0] 
+
+		if (right_check){
+			cond3 = move_from[1]+1*direction == move_to[1] && move_from[0]+1 == move_to[0]
+
+		} else {
+			cond3 = false
+		}
+		if (left_check){
+			cond4 = move_from[1]+1*direction == move_to[1] && move_from[0]-1 == move_to[0]
+
+		} else {
+			cond4 = false
+		}
+
+		return (cond1 || cond2 || cond3 || cond4)
+
+
+	
+	}
+
+	function movePiece(row,column){
+
+		if (positions_selected == 1){
+			positions_selected = 0;
+			move_to = [row-1,column-1];
+			if (move_from[0] == move_to[0] && move_from[1] == move_to[1]){
+				positions_selected = 1
+				return false
+			}
+			if (checkMove()){
+			//console.log(`piece: ${board[move_to[1]].positions[move_to[1]]}, row: ${move_to[1]}, column: ${move_to[0]}`)
+			//console.log(`piece: ${board[move_from[1]].positions[move_from[1]]}, row: ${move_from[1]}, column: ${move_from[0]}`)
+			board[move_to[1]].positions[move_to[0]] = board[move_from[1]].positions[move_from[0]];
+			board = board
+			board[move_from[1]].positions[move_from[0]] = "  ";
+			board = board
+			turn = !turn
+			
+			if (turn == false){
+				move_no++
+			}
+			return true
+		} else{
+			console.log("invalid move")
+			return false
+		}
+		} else{
+		
+			positions_selected++;
+			move_from = [row-1,column-1];
+			return false
+		}
+	}
+
+	//$: console.log(board)
+
+	function selectCell(row,column){
+
+console.log(row)
+console.log(column)
+
+	}
+$: console.log(`move_from: ${move_from}`)
+$: console.log(`move_to: ${move_to}`)
+
+</script>
+
+{#each board as row}
+<div class="row">
+	{#each row.positions as position, i}
+	<div class = "cell" on:click="{() => movePiece(i+1,row.row)}">
+	{position}
+
+</div>
+	{/each}
+	</div>
+{/each}
+
+<style>
+	.row {
+		display: flex;
+		margin: 4px 0;
+	}
+	.cell {
+		width: 5px;
+		height: 5px;
+		flex-shrink: 0;
+		text-align: center;
+		padding: 30px;
+		border: 3px solid black;
+	}	
+
+</style>
---
+++ src/store.js
@@ -0,0 +1,14 @@
+import {writable} from 'svelte/store'
+
+																
+
+export const board = [
+{row: 1, positions: ['RW','NW','BW','QW','KW','BW','NW','RW']},
+{row: 2, positions: ['PW','PW','PW','PW','PW','PW','PW','PW']},
+{row: 3, positions: ['  ','  ','  ','  ','  ','  ','  ','  ']},
+{row: 4, positions: ['  ','  ','  ','  ','  ','  ','  ','  ']},
+{row: 5, positions: ['  ','  ','  ','  ','  ','  ','  ','  ']},
+{row: 6, positions: ['  ','  ','  ','  ','  ','  ','  ','  ']},
+{row: 7, positions: ['PB','PB','PB','PB','PB','PB','PB','PB']},
+{row: 8, positions: ['RB','NB','BB','QB','KB','BB','NB','RB']},
+];
